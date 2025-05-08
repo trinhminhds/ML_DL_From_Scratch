8a431b43be01f012b222b840da7c0735ba758205
trinhminhds <trinhngocminh1999@gmail.com>
2025-05-08 17:26:37 +0700

    add RNN

--- RNN/RNN.py
+++ RNN/RNN.py
@@ -166,6 +166,111 @@ class RNN:
 
     def backward(self, x, a, y_preds, targets):
         """
-        Computes the backward pass of the RNN.
+        Implement the backward pass of the RNN.
 
+        Arguments:
+            x --(dict) of input characters (as one-hot encoding vectors) for each time step, shape of (vocab_size, sequence_length)
+            a --(dict) of hidden state vectors for each time step, shape (hidden_size, sequence_length)
+            y_pred --(dict) of output probabilities for vectors (after softmax) for each time step, shape of (vocab_size, sequence_length)
+            targets --(list) of integer targets characters (indices of characters in the vocabulary) for each time step, shape (1, sequence_length)
+
+        Returns:
+            None
         """
+        # Initialize derivative of hidden state for the last time-step
+        da_next = np.zeros_like(a[0])
+
+        # Loop through the input sequence backwards
+        for t in reversed(range(len(self.X))):
+            # Calculate derivative of output probability vector
+            dy_preds = np.copy(y_preds[t])
+            dy_preds[targets[t]] -= 1
+
+            # Calculate derivative of hidden state
+            da = np.dot(self.Waa.T, da_next) + np.dot(self.Wya.T, dy_preds)
+            dtanh = 1 - np.power(a[t], 2)
+            da_unactivated = dtanh * da
+
+            # Calculate gradients
+            self.dba += da_unactivated
+            self.dWax += np.dot(da_unactivated, x[t].T)
+            self.dWaa += np.dot(da_unactivated, a[t - 1].T)
+
+            # Update derivative of hidden state for the next iteration
+            da_next = da_unactivated
+
+            # Calculate gradients for output weight matrix
+            self.dWya += np.dot(dy_preds, a[t].T)
+
+            # Clip gradients to avoid exploding gradients
+            for grad in [self.dWax, self.dWaa, self.dWya, self.dba, self.dby]:
+                np.clip(grad, -1, 1, out=grad)
+
+    def loss(self, y_preds, targets):
+        """
+        Computes the cross entropy loss for a given sequence of predicted probabilities and true targets.
+
+        Parameters:
+            y_preds (ndarray): Array of shape (seq_length, vocab_size) containing predicted probabilities for each time step.
+            targets (ndarray): Array of shape (seq_length, 1) containing the true targets for each time step.
+
+        Returns:
+            float: Cross-entropy loss.
+        """
+        # Calculate cross-entropy loss.
+        return sum(-np.log(y_preds[t][targets[t], 0]) for t in range(len(self.X)))
+
+    def adamw(self, beta1=0.9, beta2=0.999, epsilon=1e-8, L2_reg=1e-4):
+        """
+        Updates the RNN's parameters using the AdamW optimizer algorithm.
+        """
+        # AdamW update for Wax
+        self.mWax = beta1 * self.mWax + (1 - beta1) * self.dWax
+        self.vWax = beta1 * self.vWax + (1 - beta2) * np.square(self.dWax)
+        m_hat = self.mWax / (1 - beta1)
+        v_hat = self.vWax / (1 - beta2)
+        self.Wax -= self.learning_rate * (
+            m_hat / (np.sqrt(v_hat) + epsilon) + L2_reg * self.Wax
+        )
+
+        # AdamW update for Waa
+        self.mWaa = beta1 * self.mWaa + (1 - beta1) * self.dWaa
+        self.vWaa = beta2 * self.vWaa + (1 - beta2) * np.square(self.dWaa)
+        m_hat = self.mWaa / (1 - beta1)
+        v_hat = self.vWaa / (1 - beta2)
+        self.Waa -= self.learning_rate * (
+            m_hat / (np.sqrt(v_hat) + epsilon) + L2_reg * self.Waa
+        )
+
+        # AdamW update to Wya
+        self.mWya = beta1 * self.mWya + (1 - beta1) * self.dWya
+        self.vWya = beta2 * self.vWya + (1 - beta2) * np.square(self.dWya)
+        m_hat = self.mWya / (1 - beta1)
+        v_hat = self.vWya / (1 - beta2)
+        self.Wya -= self.learning_rate * (
+            m_hat / (np.sqrt(v_hat) + epsilon) + L2_reg * self.Wya
+        )
+
+        # AdamW update for ba
+        self.mba = beta1 * self.mba + (1 - beta1) * self.dba
+        self.vba = beta2 * self.vba + (1 - beta2) * np.square(self.dba)
+        m_hat = self.mba / (1 - beta1)
+        v_hat = self.vba / (1 - beta2)
+        self.ba -= self.learning_rate * (
+            m_hat / (np.sqrt(v_hat) + epsilon) + L2_reg * self.ba
+        )
+
+        # AdamW update for by
+        self.mby = beta1 * self.mby + (1 - beta1) * self.dby
+        self.vby = beta2 * self.vby + (1 - beta2) * np.square(self.dby)
+
+        def sample(self):
+            """
+            Sample a sequence of characters from the RNN
+
+            Arguments:
+                None
+
+            Returns:
+                list: A list of integers representing the generated sequence.
+            """
